3b5980676a238f65125dd586f56777a8a2a2a360
WierdoTheFarmer <gormollari@hotmail.com>
2021-12-14 16:29:07 +0200

    decode item dont need anymore... i think its TRUE lol

--- typeID.py
+++ typeID.py
@@ -1,5 +1,4 @@
 import json
-import urllib.parse
 
 
 
@@ -8,7 +7,5 @@ class TypeID:
     def getID(self, typeName):
         with open("typeid.json") as file:
             typeID_DB = json.load(file)
-#       typeName = typeName.replace("%20", " ")
-        typeIdName = urllib.parse.unquote(typeName) 
         typeID = typeID_DB[typeName]["0"]
         return(typeID)
